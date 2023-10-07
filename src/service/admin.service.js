21f99271caa7b85965032c75cce754a3419d4cee
Gift <Giftibe62@gmail.com>
2023-10-07 12:54:01 +0100

    added login and verify email feature

--- src/service/admin.service.js
+++ src/service/admin.service.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 const generate_Template = require('../utils/template');
 const Buyer = require('../model/buyer.model');
 const Seller = require('../model/seller.model');
+const { findByIdAndDelete } = require('../model/house.model');
 const { subject1, secret } = process.env
 
 
@@ -24,10 +25,19 @@ class AdminServices {
                 email: email,
                 password: password
             })
+            const verification_Token = jwt.sign({ email }, secret, {
+                expiresIn: "30m",
+            });
+            const Link = `https://propell-ten.vercel.app/verifyMail/${encodeURIComponent(
+                verification_Token
+            )}`;
+            const htmlFileDir = path.join(__dirname, "../client/verify-1.html");
 
             //send email to verify account
-            generate_Template(email, secret)
+            // using nodemailer to send the email
+            generate_Template(Link, htmlFileDir)
             mailer({ subject: subject1, template: generate_Template, email: email })
+
             return res.status(201).send({
                 message: MESSAGES.USER.CREATED,
                 success: true,
@@ -55,7 +65,16 @@ class AdminServices {
 
             //check if users' email is verified
             if (findUser.isVerified === false) {
-                generate_Template(email, secret)
+                const verification_Token = jwt.sign({ email }, secret, {
+                    expiresIn: "30m",
+                });
+                const Link = `https://propell-ten.vercel.app/verifyMail/${encodeURIComponent(
+                    verification_Token
+                )}`;
+                const htmlFileDir = path.join(__dirname, "../client/verify-1.html");
+
+                //send email to verify account
+                generate_Template(Link, htmlFileDir)
                 mailer({ subject: subject1, template: generate_Template, email: email })
                 return res.status(201).send({
                     message: 'MESSAGES.USER.VERIFY_EMAIL',
@@ -85,7 +104,7 @@ class AdminServices {
     async getAUser(data) {
         try {
             const { id } = data
-            const findUser = await Admin.find({ _id: id }, { _id: 1, password: 0 })
+            const findUser = await Admin.findById({ _id: id }, { _id: 1, password: 0 })
             if (!findUser) {
                 return res.status(403).send({
                     message: 'No User Found',
@@ -157,7 +176,7 @@ class AdminServices {
     async getABuyer(data) {
         try {
             const { id } = data
-            const User = await Buyer.find({ _id: id }, { _id: 1, password: 0 })
+            const User = await Buyer.findById({ _id: id }, { _id: 1, password: 0 })
             if (!User) {
                 return res.status(403).send({
                     message: 'MESSAGES.USER.NO_USER',
@@ -180,7 +199,7 @@ class AdminServices {
     async getASeller(data) {
         try {
             const { id } = data
-            const User = await Seller.find({ _id: id }, { _id: 1, password: 0 })
+            const User = await Seller.findById({ _id: id }, { _id: 1, password: 0 })
             if (!User) {
                 return res.status(403).send({
                     message: 'MESSAGES.USER.NO_USER',
@@ -201,15 +220,14 @@ class AdminServices {
 
 
     //update an Admin
-    async updateAUser(req, res) {
+    async updateAdmin(data) {
         try {
-            const { id } = req.params;
-            const { firstName, lastName, mobile } = req.body;
+            const { id, firstName, lastName, mobile } = data;
             // Check if valid id
 
-            const findUser = await Admin.find({ _id: id });
+            const findUser = await Admin.findById({ _id: id });
             if (findUser) {
-                const updated = await Admin.update({ _id: id }, {
+                const updated = await Admin.findByIdAndUpdate({ _id: id }, {
                     firstName: firstName,
                     lastName: lastName,
                     mobile: mobile
@@ -246,7 +264,7 @@ class AdminServices {
     //     @desc    Handles user logout
     //     *  @access  Private
 
-    async loggedOut() {
+    async loggedOutAdmin() {
         try {
             const token = "";
             await res.cookie("token", token, { httpOnly: true });
@@ -264,11 +282,124 @@ class AdminServices {
     }
 
 
-    //verify account
-    //logout
-\    //block an account
+    //    @route   POST /api/v1/user/reset-password
+    //     @desc    Sends reset password link via mail
+    //     *  @access  Public
+
+    async sendAdminResetPasswordLink(data) {
+        try {
+            //check if the email exist in the database
+            const { email } = data;
+            const userEmail = await Admin.find({ email: email });
+            if (!userEmail) {
+                return res.status(404).send({
+                    message: 'MESSAGES.USER.EMAIL_NOTFOUND',
+                    success: false,
+                });
+            }
+            //if the email exists send
+            const payload = {
+                email: userEmail.email,
+                id: userEmail.id,
+                password: userEmail.password
+            };
+
+            const token = jwt.sign(payload, secret, { expiresIn: "30m" });
+            const Link = `https://propell-ten.vercel.app/user/reset-password/${encodeURIComponent(
+                userEmail.id
+            )}/${encodeURIComponent(token)}`;
+
+            //email sending
+            const htmlFileDir = path.join(__dirname, "../client/password.html");
+            const subject2 = "Reset Password";
+
+            // using nodemailer to send the email
+            generate_Template(Link, htmlFileDir)
+            mailer({ subject: subject2, template: generate_Template, email: email })
+
+            return res.status(201).send({
+                success: true,
+                message: 'MESSAGES.USER.EMAIL_SENT',
+            });
+        } catch (error) {
+            return res.status(500).send({
+                message: 'MESSAGES.USER.SERVER_ERROR' + error,
+                success: false,
+            });
+        }
+    }
+
+
+    //      @route  GET /api/v1/user/reset-password/:id/:token
+    //     @desc  Verifies link sent to the email checking the token and if the user exists using their ID
+    //     *  @access  Unique
+
+    async checkAdminResetPasswordLink(data) {
+        try {
+            const { id, token } = data;
+            //check if a user with the id exist in db
+            const checkUser = await Admin.findById({ _id: id });
+            if (!checkUser) {
+                return res.status(401).send({
+                    message: 'MESSAGES.USER.ACCOUNT_NOT_REGISTERED',
+                    success: false,
+                });
+            }
+
+            try {
+                const secret = process.env.SECRET_KEY;
+                jwt.verify(token, secret);
+                return res.status(200).send({
+                    message: 'MESSAGES.USER.VALID_LINK',
+                    success: true,
+                });
+            } catch (error) {
+                return res.status(403).send({
+                    message: 'MESSAGES.USER.INVALID_LINK' + error,
+                    success: false,
+                });
+            }
+        } catch (error) {
+            return res.status(500).send({
+                message: 'MESSAGES.USER.SERVER_ERROR' + error,
+                success: false,
+            });
+        }
+    }
+
+    //      @route  PATCH /api/v1/user/setpassword/:id
+    //     @desc    updates the password field
+    //     *  @access  Private
+
+    async updatePassword(data) {
+        try {
+            //check if  the email exist
+            const{id, password} = data;
+
+            const userfound = await Admin.findById({ _id: id });
+            if (!userfound) {
+                return res.status(404).send({
+                    message: 'MESSAGES.USER.EMAIL_NOTFOUND',
+                    success: false,
+                });
+            }
+            //generate new password and update it
+            await Admin.findByIdAndDelete(id, { password: password });
+            return res.status(201).send({
+                message: 'MESSAGES.USER.PASSWORD_UPDATED',
+                success: true,
+            });
+        } catch (error) {
+            return res.status(500).send({
+                message: 'MESSAGES.USER.SERVER_ERROR' + error,
+                success: false,
+            });
+        }
+    }
+
+    //block an account
     //unblock an account
 
 }
 
-module.exports = AdminServices()
+module.exports = new AdminServices()
--- src/utils/template.js
+++ src/utils/template.js
@@ -1,16 +1,8 @@
 const jwt = require("jsonwebtoken");
-const generateTemplate = (email, secret) => {
-    const verification_Token = jwt.sign({ email }, secret, {
-        expiresIn: "30m",
-    });
-    const verificationLink = `https://propell-ten.vercel.app/verifyMail/${encodeURIComponent(
-        verification_Token
-    )}`;
-
+const generateTemplate = (Link, htmlFileDir) => {
     //email sending
-    const htmlFileDir = path.join(__dirname, "../client/verify-1.html");
     const htmlFiler = fs.readFileSync(htmlFileDir, "utf8");
-    const template = htmlFiler.replace("{{VERIFICATIONLINK}}", verificationLink);
+    const template = htmlFiler.replace("{{LINK}}", Link);
     return template
 }
 module.exports = generateTemplate()
