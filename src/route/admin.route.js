631c9e471e2b4ae0dd278489d4cd8e0331265063
Gift <Giftibe62@gmail.com>
2023-10-08 23:56:50 +0100

    feat: added validation to routes

--- src/route/admin.route.js
+++ src/route/admin.route.js
@@ -1,6 +1,15 @@
 const { Router } = require('express')
 const adminRouter = Router()
 const adminController = require('../controller/admin.controller')
+const validation = require('../validation/validate.admin')
+const isValidUUID = require('../validation/verifyID')
+
+const {
+    validate_Account_Creation_Inputs,
+    validate_Login_Inputs,
+    validate_Admin_Update
+} = validation
+
 const {
     signUp_Admin,
     login_Admin,
@@ -15,15 +24,15 @@ const {
 } = adminController
 
 
-adminRouter.post('/admin/register', signUp_Admin)
-adminRouter.post('/admin/login', login_Admin)
+adminRouter.post('/admin/register', validate_Account_Creation_Inputs, signUp_Admin)
+adminRouter.post('/admin/login', validate_Login_Inputs, login_Admin)
 adminRouter.post('/admin/logout', logout_Admin)
 adminRouter.post('/admin/resetpassword', sendAdmin_ResetPassword_Link)
-adminRouter.get('/admin/:customAdminId', get_Admin)
+adminRouter.get('/admin/:customAdminId', isValidUUID, get_Admin)
 adminRouter.get('/admin/users', get_All_User)
-adminRouter.get('/admin/user/:customUserId', get_A_User)
-adminRouter.get('/admin/paswordcheck/:customAdminId/:token', checkAdmin_ResetPassword_Link)
-adminRouter.patch('/admin/setpassword/:customAdminId', update_password)
-adminRouter.patch('/admin/updateAdmin/:customAdminId', update_Admin)
+adminRouter.get('/admin/user/:customUserId', isValidUUID, get_A_User)
+adminRouter.get('/admin/paswordcheck/:customAdminId/:token', isValidUUID, checkAdmin_ResetPassword_Link)
+adminRouter.patch('/admin/setpassword/:customAdminId', isValidUUID, update_password)
+adminRouter.patch('/admin/updateAdmin/:customAdminId', isValidUUID, validate_Admin_Update, update_Admin)
 
 module.exports = adminRouter
---
+++ src/validation/validate.user.js
@@ -0,0 +1,84 @@
+const Joi = require('joi')
+
+// CREATION OF JOI SCHEMAS
+
+const createAccountSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+})
+
+
+const loginUserSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+})
+
+const updateUser = Joi.object({
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    mobile: Joi.number(),
+    company_Name: Joi.string(),
+    isVerified: Joi.boolean(),
+    houses: Joi.string(),
+    isBlocked: Joi.boolean(),
+    savedItem: Joi.array()
+})
+
+
+// VALIDATIONS USING CREATED SCHEMAS
+
+const validate_Account_Creation_Inputs = (req, res, next) => {
+    try {
+        const validateInput = createAccountSchema.validate(req.body)
+
+        if (validateInput.error) {
+            return res.status(404).send({
+                success: false,
+                status: 'failed',
+                errormessage: validateInput.error.details[0].message
+            })
+        } else {
+            next()
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+const validate_Login_Inputs = (req, res, next) => {
+    try {
+        const validateInput = loginUserSchema.validate(req.body)
+
+        if (validateInput.error) {
+            return res.status(404).send({
+                success: false,
+                status: 'failed',
+                errormessage: validateInput.error.details[0].message
+            })
+        } else {
+
+            next()
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+const validate_User_Update = (req, res, next) => {
+    try {
+        const validateInput = updateUser.validate(req.body)
+
+        if (validateInput.error) {
+            return res.status(404).send({
+                success: false,
+                status: 'failed',
+                errormessage: validateInput.error.details[0].message,
+            })
+        } else {
+            next()
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+module.exports = { validate_Account_Creation_Inputs, validate_Login_Inputs, validate_User_Update }
